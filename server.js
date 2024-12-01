e67a885d8ff370263ce94441e024286e76b1cc2e
llccrr <loic1.renaud@hotmail.fr>
2024-12-02 00:53:45 +0100

    Update documentation & add api tester interface

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const {listen} = require('./api');
-listen(3020, () => {
-    console.log(`API Elo en écoute sur le port ${3020}`);
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Servir les fichiers statiques du dossier public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Importer les routes de l'API
+const apiRoutes = require('./api');
+app.use('/', apiRoutes);
+
+// Démarrer le serveur
+const PORT = 3020;
+app.listen(PORT, () => {
+    console.log(`API Elo en écoute sur le port ${PORT}`);
 });
